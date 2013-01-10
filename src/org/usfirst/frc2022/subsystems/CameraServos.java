91571ffa1ad5ad87b1f95951d1172cddc489ffb6
nmagerko <nmagerko@gmail.com>
2013-01-10 14:38:57 -0600

    Added JavaDocs to the camera subsystem

--- src/org/usfirst/frc2022/subsystems/CameraServos.java
+++ src/org/usfirst/frc2022/subsystems/CameraServos.java
@@ -3,38 +3,89 @@ package org.usfirst.frc2022.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.Servo;
 
+/**
+ * This subsystem is for use with the camera on Clifford. It uses two
+ * servos, one for changing the camera's pitch and another for the 
+ * camera's rotation. *
+ */
+
 public class CameraServos extends Subsystem {
 	
 	private Servo pitchServo;
 	private Servo rotateServo;
 	
+	/**
+	 * Creates a new CameraServos subsystem that assumes the default
+	 * digital servos module.
+	 * 
+	 * @param pitchChannel - channel for the pitch servo
+	 * @param rotateChannel - channel for the rotate servo
+	 * 
+	 */
 	public CameraServos(int pitchChannel, int rotateChannel){
 		pitchServo = new Servo(pitchChannel);
 		rotateServo = new Servo(rotateChannel);		
 	}
 	
+	/**
+	 * Creates a new CameraServos subsystem that specifies the 
+	 * digital servo module.
+	 * 
+	 * @param pitchSlot - slot for the pitch servo
+	 * @param pitchChannel - channel for the pitch servo
+	 * @param rotateSlot - slot for the rotate servo
+	 * @param rotateChannel - channel for the rotate servo
+	 * 
+	 */
 	public CameraServos(int pitchSlot, int pitchChannel, int rotateSlot, int rotateChannel){
 		pitchServo = new Servo(pitchSlot, pitchChannel);
 		rotateServo = new Servo(rotateSlot, rotateChannel);
 		
 	}
 	
+	/**
+	 * Sets the angle for the pitch servo
+	 * 
+	 * @param angle - angle to pitch
+	 * 
+	 */
 	public void setPitchAngle(double angle){
 		pitchServo.setAngle(angle);
 	}
 	
+	/**
+	 * Sets the angle for the rotate servo
+	 * 
+	 * @param angle - angle to rotate
+	 * 
+	 */
 	public void setRotateAngle(double angle){
 		rotateServo.setAngle(angle);
 	}
 	
+	/**
+	 * Gives the angle for the pitch servo
+	 * 
+	 * @return getAngle() - angle of the pitch servo
+	 * 
+	 */
 	public double getPitchAngle(){
 		return pitchServo.getAngle();
 	}
 	
+	/**
+	 * Gives the angle for the rotate servo
+	 * 
+	 * @return getAngle() - angle of the rotate servo
+	 * 
+	 */
 	public double getRotateAngle(){
 		return rotateServo.getAngle();
 	}
 	
+	/**
+	 * Default command for the CameraServos subsystem
+	 */
 	protected void initDefaultCommand() {
 		
 	}
