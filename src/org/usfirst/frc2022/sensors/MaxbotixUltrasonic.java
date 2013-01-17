ceb7f91608611fde8719bb9cf4714e4bfdeef876
Paul Rosa <prosa100@gmail.com>
2013-01-16 20:06:52 -0600

    Added comments

--- src/org/usfirst/frc2022/sensors/MaxbotixUltrasonic.java
+++ src/org/usfirst/frc2022/sensors/MaxbotixUltrasonic.java
@@ -2,20 +2,36 @@ package org.usfirst.frc2022.sensors;
  
 import edu.wpi.first.wpilibj.*;
 //PWH
-//Voltage * 106.23+0.3793
+/**
+* This is for making the Maxbotix Ultrasound stuff work!
+* It's good.
+*/
 public class MaxbotixUltrasonic extends SensorBase {
  AnalogChannel channel;
    
-    
+    /**
+	* Maxbotix Ultrasonic.
+	* Create it. Good. Now use it.
+	*/
     public MaxbotixUltrasonic(int channel){
     	this.channel=new AnalogChannel(channel);
     }
     
     // Just get the voltage.
+	
+	/**
+	* Get the raw voltage from the sensor.
+	* If you're using this, you're doing it wrong.
+	*/
     double GetVoltage() {
         return channel.getVoltage();
     }
-    double GetDistance(){
+    
+    /**
+	* Get the distance!!!
+	* Use this!
+	*/
+	double GetDistance(){
     	return GetVoltage()*106.23+0.3793;
     }
 }
